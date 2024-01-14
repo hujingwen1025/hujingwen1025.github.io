a06da840bf2420c8823abd2f8b95748f29ab8327
Jingwen <91361904+hujingwen1025@users.noreply.github.com>
2024-01-14 10:33:13 +0800

    Update mainsetup.sh

--- ish/setups/vnc/i3/mainsetup.sh
+++ ish/setups/vnc/i3/mainsetup.sh
@@ -2,5 +2,12 @@
 echo 'Running i3 x11 VNC setup script...'
 wget -O - https://hujingwen1025.github.io/ish/setups/vnc/i3/i3vncsetup.sh | sh
 wget https://hujingwen1025.github.io/ish/setups/vnc/i3/starti3vnc.sh
+clear
+echo '==i3 x11 VNC SETUP=='
 echo 'Installation done!'
 echo 'Run i3 x11 VNC by passing "sh starti3vnc.sh"'
+echo ' '
+echo 'Run this command immediately: '
+echo 'x11vnc -storepasswd'
+echo 'This command will set a password for your VNC, you will not be able to see the password when you enter it. Choose y when instructed!'
+echo ' '
